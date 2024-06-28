21c20e6f27bb6cefb3676375f5bf9f90493b41be
Diwakar Kumar <114383973+Diwakar03@users.noreply.github.com>
2024-06-28 15:13:53 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [server]\n\
+headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
-handless = true\n\
 \n\
-" > ~/.streamlit/congig.toml
+" > ~/.streamlit/config.toml
+
+python -c "import nltk; nltk.download('stopwords'); nltk.download('wordnet')"
